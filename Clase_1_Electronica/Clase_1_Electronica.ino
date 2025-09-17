5bbce9fcb9814c2e764e038484026fa710add521
Ssp_Projects <ssalazarp@unal.edu.co>
2025-09-16 20:05:30 -0500

    Initial commit

---
+++ Clase_1_Electronica/Clase_1_Electronica.ino
@@ -0,0 +1,18 @@
+//ESP32 DEV MODULE, para usar con ESP32
+
+int pin2=2;
+int pin3=3;
+int pinAnalog=A0;
+
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+pinMode(pin2, OUTPUT);
+pinMode(pin3, INPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
